d0b5e334bce416598a25f277a078cdc4e51dc2be
metalDogFish <58714411+metalDogFish@users.noreply.github.com>
2026-02-03 22:36:22 -0500

    Update particle.js

--- particle.js
+++ particle.js
@@ -16,8 +16,8 @@ class Particle {
   }
 
   follow(vectors) {
-    var x = floor(this.pos.x / scl);
-    var y = floor(this.pos.y / scl);
+    var x = this.p.floor(this.pos.x / scl);
+    var y = this.p.floor(this.pos.y / scl);
     var index = x + y * cols;
     var force = vectors[index];
     this.applyForce(force);
