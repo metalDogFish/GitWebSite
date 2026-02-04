229f85b27f07e357b7a10abaaf324ed4fa1049ee
metalDogFish <58714411+metalDogFish@users.noreply.github.com>
2026-02-04 12:02:49 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,14 +23,14 @@ p.setup = function(){
   //reference container in index.html
   canvas.parent('sketch-container');
  
-  cols = this.p.floor(p.width / scl);
-  rows = this.p.floor(p.height / scl);
+  cols = p.floor(p.width / scl);
+  rows = p.floor(p.height / scl);
 
   flowfield = new Array(cols * rows);
 
-  fr = this.p.createP("");
+  fr = p.createP("");
   fr.style("font-size", "16px");
-  let col = this.p.color(255);
+  let col = p.color(255);
   fr.style("color", col);
 
   for (let i = 0; i < 200; i++) {
@@ -38,17 +38,17 @@ p.setup = function(){
     particles[i] = new Particle(p, p.random(p.width),p.random(p.height));
   }
 
-  chBox = this.p.createCheckbox("z dimension",true);
+  chBox = p.createCheckbox("z dimension",true);
   chBox.style("color", col);
  chBox.parent('sketch-container');
-  chBox2 = this.p.createCheckbox("curtain",true);
+  chBox2 = p.createCheckbox("curtain",true);
   chBox2.style("color", col);
   chBox2.parent('sketch-container');
 }
 
 //function draw() {
  p.draw = function(){
-  this.p.background(20, 90); //makes particles appear a bit translucent
+  p.background(20, 90); //makes particles appear a bit translucent
   // randomSeed(10);
   var yoff = 0;
   for (var y = 0; y < rows; y++) {
@@ -56,7 +56,7 @@ p.setup = function(){
     for (var x = 0; x < cols; x++) {
       var index = x + y * cols;
       //var n = noise(xoff, yoff) * 255;
-      var angle = this.p.noise(xoff, yoff, zoff) * this.p.TWO_PI * 2; //higher number induces more vortices.
+      var angle = p.noise(xoff, yoff, zoff) * p.TWO_PI * 2; //higher number induces more vortices.
       var v = p.Vector.fromAngle(angle);
       v.setMag(0.5);
       flowfield[index] = v;
@@ -64,12 +64,12 @@ p.setup = function(){
 
       if (!chBox2.checked()) {
         //green kelp
-        this.p.stroke(20, 130, 30, 80);
-        this.p.push();
-        this.p.translate(x * scl, y * scl);
-        this.p.rotate(angle); //v.heading());
-        this.p.line(0, 0, scl * 1.4, 0);
-         this.p.pop();
+        p.stroke(20, 130, 30, 80);
+        p.push();
+        p.translate(x * scl, y * scl);
+        p.rotate(angle); //v.heading());
+        p.line(0, 0, scl * 1.4, 0);
+         p.pop();
       }
       //fill(random(255));
       //fill(v);
@@ -79,7 +79,7 @@ p.setup = function(){
     yoff += inc;
     //Is checkbox1 checked? Say so.
     if (chBox.checked()) {
-      this.p.text("Flowing", 20, 40);
+      p.text("Flowing", 20, 40);
       zoff += 0.00003;
     }
     // zoff += 0.0003; //z acts like time dimension
@@ -94,17 +94,17 @@ p.setup = function(){
   //pop();
   //stroke(240);
   // fill(240);
-  fr.html(this.p.floor(this.p.frameRate()));
+  fr.html(p.floor(this.p.frameRate()));
 }
 
 //function mouseClicked(){
  p.mousePressed = function(){
-  let mx = this.p.mouseX;
-  let my = this.p.mouseY;
+  let mx = p.mouseX;
+  let my = p.mouseY;
   
   let nup = new Particle(p,mx,my)
   particles.push(nup);
-  this.p.print('new particle')
+  p.print('new particle')
 }
 };
 
