c077b721cb70c6407081f54d36da37bb4ae49f37
metalDogFish <58714411+metalDogFish@users.noreply.github.com>
2026-02-08 21:32:40 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,108 +1,95 @@
-// Flowfields with persistant particles.
-//flowfields are created using Perlin noise mapped to a 2d grid.
-//z dimension toggle allows flowfield to move through time.
-//curtain toggle hides background grid to focuss on particle movement.
-//particles get their directional movement from surrounding flowfield. 
-//shawn nykwist
+//Slime Molds- changing the sensor angle results in wild variations..
+//----------------------------------------s.nykwist
 
-var inc = 0.1;
-var scl = 10;
-var cols, rows;
-var fr;
-
-var zoff = 0;
-
-var particles = [];
-
-var flowfield;
-var chBox, chBox2;
+let molds = [];
+let num = 4000;
+let d;
+let slider;
+let btnUp, btnDn;
+//let isPaused = false;
 
 function setup() {
- 
-  let canvas = createCanvas(300, 300);
-  //reference container in index.html
+   let canvas = createCanvas(400, 400);
+ //reference container in index.html
   canvas.parent('sketch-container');
- 
-  cols = floor(width / scl);
-  rows = floor(height / scl);
+  //referenced by mold
+  angleMode(DEGREES);
+  d = pixelDensity();
 
-  flowfield = new Array(cols * rows);
-
-  fr = createP("");
-  fr.style("font-size", "16px");
-  let col = color(255);
-  fr.style("color", col);
-
-  for (let i = 0; i < 200; i++) {
-    particles[i] = new Particle();
-    console.log("Drawing particle", i);
+  //create an array of mold
+  for (let i = 0; i < num; i++) {
+    molds[i] = new Mold();
   }
 
-  chBox = createCheckbox("z dimension",true);
-  chBox.style("color", col);
- chBox.parent('sketch-container');
-  chBox2 = createCheckbox("curtain",true);
-  chBox2.style("color", col);
-  chBox2.parent('sketch-container');
+  //set up user interface--------------
+  slider = createSlider(0, 360, 45, 5);
+  //slider.value = 45;
+ slider.parent('sketch-container');
+  slider.mouseClicked(sliderFunc);
+  btnUp = createButton(" + ");
+ btnup.parent('sketch-container');
+  btnUp.mouseClicked(butFuncUp);
+  btnDn = createButton(" - ");
+ btnDn.parent('sketch-container');
+  btnDn.mouseClicked(butFuncDn);
+  //-------------------end interface
 }
 
 function draw() {
-  background(20, 90); //makes particles appear a bit translucent
-  // randomSeed(10);
-  var yoff = 0;
-  for (var y = 0; y < rows; y++) {
-    var xoff = 0;
-    for (var x = 0; x < cols; x++) {
-      var index = x + y * cols;
-      //var n = noise(xoff, yoff) * 255;
-      var angle = noise(xoff, yoff, zoff) * TWO_PI * 2; //higher number induces more vortices.
-      var v = p5.Vector.fromAngle(angle);
-      v.setMag(0.5);
-      flowfield[index] = v;
-      xoff += inc;
+  //important that background is set to black, alpha = 5;
+  background(0, 5);
 
-      if (!chBox2.checked()) {
-        //green kelp
-        stroke(20, 130, 30, 80);
-        push();
-        translate(x * scl, y * scl);
-        rotate(angle); //v.heading());
-        line(0, 0, scl * 1.4, 0);
-         pop();
-      }
-      //fill(random(255));
-      //fill(v);
-      // rect(x * scl, y * scl, scl, scl);
-     
-    }
-    yoff += inc;
-    //Is checkbox1 checked? Say so.
-    if (chBox.checked()) {
-      text("Flowing", 20, 40);
-      zoff += 0.00003;
-    }
-    // zoff += 0.0003; //z acts like time dimension
-  }
+  //needed to detect pixel color
+  loadPixels();
 
-  for (let i = 0; i < particles.length; i++) {
-    particles[i].follow(flowfield);
-    particles[i].update();
-    particles[i].edges();
-    particles[i].show();
+  //move and display molds
+  for (let i = 0; i < num; i++) {
+    //trigger to stop mold growth
+    if (key == "s") {
+      // If "s" key is pressed, molds stop moving
+      molds[i].stop = true;
+      updatePixels();
+      noLoop();
+      print("frozen");
+    }
+
+    molds[i].update();
+    molds[i].display();
   }
-  //pop();
-  //stroke(240);
-  // fill(240);
-  fr.html(floor(frameRate()));
 }
 
-function mouseClicked(){
-  let mx = mouseX;
-  let my = mouseY;
-  
-  let p = new Particle(mx,my)
-  particles.push(p);
-  print('new particle')
+function butFuncUp() {
+  // slider.value += 5;
+  //slider.setValue(5);
+  //get original slider value
+  let sv = slider.value();
+  //try to set slider adding 5 to value
+  slider.value(sv + 5);
+  //call slider func that changes mold
+  sliderFunc();
+
+  print("increasing to " + (sv + 5));
 }
-//https://www.youtube.com/watch?v=BjoM9oKOAKY&list=PLRqwX-V7Uu6bgPNQAdxQZpJuJCjeOr7VD&index=6
-//extending this to mimick ocean kelp moving with the currents..
+
+function butFuncDn() {
+  //get original slider value
+  let sv = slider.value();
+  //try to set slider adding 5 to value
+  slider.value(sv - 5);
+  //call slider func that changes mold
+  sliderFunc();
+
+  print("decreasing to " + (sv - 5));
+}
+
+function sliderFunc() {
+  let sv = slider.value();
+  console.log("slider reset " + sv);
+  for (let m of molds) {
+    //parameter takes slider value
+    m.resetValue(sv);
+  }
+}
+
+//2024/11/23-discovering sl
+
