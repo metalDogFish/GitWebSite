233f3720fd6cf80d73c15954562625c5982a66fa
metalDogFish <58714411+metalDogFish@users.noreply.github.com>
2026-03-17 18:04:56 -0400

    Fix button parent assignment in sketch3.js

--- sketch3.js
+++ sketch3.js
@@ -18,7 +18,7 @@ function setup() {
   keyMax = 8;
   for (let i = 0; i < 8; i++) {
     buttons[i] = createButton("tone" + (i + 1));
-     buttons[i].parent('container3');
+
     buttons[i].size(width / keyMax, 150);
     // buttons[i].mousePressed(playTone);
     //to allow parameters in function call, use method below
