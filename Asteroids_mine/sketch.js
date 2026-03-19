b9d55a7976c825b34d242b611db20a4bcc7f58cc
metalDogFish <58714411+metalDogFish@users.noreply.github.com>
2026-03-19 11:41:23 -0400

    Update sketch.js

--- Asteroids_mine/sketch.js
+++ Asteroids_mine/sketch.js
@@ -245,7 +245,7 @@ function setup() {
   saucer = new Saucer();
   saucer.active = false;
 
-  isSoundOn = true;
+  isSoundOn = false;
 
  // let boxPhrase = new p5.Phrase("box", playBox, boxPat);
   //let drumPhrase = new p5.Phrase("drum", playDrum, drumPat);
