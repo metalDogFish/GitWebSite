7051ad3012b6576974a18424472015ebe9bb3501
metalDogFish <58714411+metalDogFish@users.noreply.github.com>
2026-03-23 11:46:24 -0400

    Update sketch.js

--- Asteroids_mine/sketch.js
+++ Asteroids_mine/sketch.js
@@ -213,7 +213,7 @@ function preload() {
 }
 ////////////////////////////////////////////////setup
 function setup() {
- let canv = createCanvas(800, 800);
+ let canv = createCanvas(700, 700);
   console.log(select("#sketch-container4"));
   canv.parent("sketch-container4");
   let canvasPos = canv.position();
